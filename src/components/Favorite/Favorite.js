925e92fce21ff938cde3852eb4c261ca19b2ed3b
mikka <mikkanthrope@gmail.com>
2018-09-10 01:59:53 -0700

    change favorites in localStorage from array to object to speed up lookup

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -7,7 +7,7 @@ export default class Favorite extends Component {
   constructor(props) {
     super(props)
     this.state={
-      favorited: window.localStorage.getItem('favorites') ? JSON.parse(window.localStorage.getItem('favorites')).includes(props.id) : false
+      favorited: window.localStorage.getItem('favorites') ? JSON.parse(window.localStorage.getItem('favorites'))[props.id] : false
     }
   }
 
@@ -19,19 +19,19 @@ export default class Favorite extends Component {
     })
     if (window.localStorage.getItem('favorites')) {
       let retrieved = JSON.parse(window.localStorage.getItem('favorites'))
-      if (retrieved.includes(id)) { // REMOVE CAR IF IT'S ALREADY IN THE FAVORITES ARRAY
-        let newFavorites = retrieved.filter(VIN => VIN !== id)
+      if (retrieved[id]) { // REMOVE CAR IF IT'S ALREADY IN THE FAVORITES OBJECT
+        delete retrieved[id]
         window.localStorage.clear()
-        window.localStorage.setItem('favorites', JSON.stringify(newFavorites))
+        window.localStorage.setItem('favorites', JSON.stringify(retrieved))
         return
       } else { // ADD CAR IF IT'S NOT ALREADY IN THE FAVORITES ARRAY
-        retrieved.push(id)
+        retrieved[id] = true
         window.localStorage.setItem('favorites', JSON.stringify(retrieved))
         return
       }
-    } else { // CREATE AND SAVE A FAVORITES ARRAY IF IT DOESN'T ALREADY EXIST
-      let favorites = []
-      favorites.push(id)
+    } else { // CREATE AND SAVE A FAVORITES OBJECT IF IT DOESN'T ALREADY EXIST
+      let favorites = {}
+      favorites[id] = true
       window.localStorage.setItem('favorites', JSON.stringify(favorites))
       return
     }

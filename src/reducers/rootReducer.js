4d6c8aa6bfa48a42f7bb71a42573c75daa83665e
mikka <mikkanthrope@gmail.com>
2019-01-18 17:59:54 -0800

    Rename app

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import cardsReducer from './carsReducer'
+import cardsReducer from './cardsReducer'
 
 const appReducer = combineReducers({
   cards: cardsReducer

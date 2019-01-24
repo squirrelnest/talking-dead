a3db6277a16334a80149668ad2d88c400490f377
mikka <mikkanthrope@gmail.com>
2019-01-24 15:01:27 -0800

    add firebase authentication

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import { Switch, Route } from 'react-router'
 import 'styles/global.css'
 import CardList from 'containers/CardList'
 import { getCards } from 'actions/cardsActions'
+import { auth, firebaseAuthProvider } from './firebase.js';
 
 export class App extends Component {
 
   constructor(props) {
     super(props);
     this.state={
+      authenticated: false
     }
   }
 
@@ -18,15 +20,36 @@ export class App extends Component {
     this.props.getCards()
   }
 
+  componentDidMount() {
+    auth.onAuthStateChanged((authenticated) => {
+      if (authenticated) {
+        this.setState({ authenticated });
+      }
+    });
+  }
+
+  login = () => {
+    auth.signInWithPopup(firebaseAuthProvider)
+      .then((result) => {
+        this.setState({
+          user: result.user
+        });
+      });
+  }
+
   render() {
 
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={ CardList } />
-          <Route exact path="/cards" component={ CardList } />
-        </Switch>
-      </BrowserRouter>
+        <BrowserRouter>
+          { this.state.authenticated ?
+          <Switch>
+            <Route exact path="/" component={ CardList } />
+            <Route exact path="/cards" component={ CardList } />
+          </Switch>
+          :
+          <button onClick={this.login}>Log In</button>
+          }
+        </BrowserRouter>
     )
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyBHZlURzUqnuFC_v4jvLC5W0AjkPMNwwTk",
+  authDomain: "talking-dead.firebaseapp.com",
+  databaseURL: "https://talking-dead.firebaseio.com",
+  projectId: "talking-dead",
+  storageBucket: "talking-dead.appspot.com",
+  messagingSenderId: "344960091762"
+};
+firebase.initializeApp(config);
+export const firebaseAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export const database = firebase.database();
+
+export default firebase;

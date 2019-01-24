f94451ac510b55b8ab4751a79c60e0a302b672cb
mikka <mikkanthrope@gmail.com>
2019-01-24 04:07:14 -0800

    more https

--- src/config.js
+++ src/config.js
@@ -6,10 +6,10 @@ let frontendHost
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === 'localhost') {
-  backendHost = '//message-list.appspot.com'
-  frontendHost = '//localhost:4000'
+  backendHost = 'https://message-list.appspot.com'
+  frontendHost = 'https://localhost:4000'
 } else if (hostname === 'message-list.appspot.com') {
-  backendHost = '//message-list.appspot.com'
+  backendHost = 'https://message-list.appspot.com'
 }
 
 export const API_HOST = `${backendHost}`
